896b87f2d2cd430694606acf9426c8a0fed06129
LeonardGracciano <100881945+LeonardGracciano@users.noreply.github.com>
2022-03-08 11:11:04 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var box 
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200, 200)
+  
+}
+
+function draw() 
+{
+  background(30);
+
+if (keyDown(RIGHT_ARROW)){
+    box.x=box.x+10
+
+
+}
+
+
+
+
+  drawSprites()
+
+  
+}
+
+
+
+
